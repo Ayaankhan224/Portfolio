fe140138bda493fbdd9b77d045170907744bbc28
Ayaan Khan <ayaankhan224271@gmail.com>
2026-05-25 20:10:43 +0530

    finalised page1

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
-// loading anim
-
-let loadingText=document.querySelector(".l2 h1");
-let loadingArr=["PREPARING THE CONTENT","warming up the pixels","brewing the content","SUMMONING THE GOOD STUFF","MAKING THINGS LOOK EXPENSIVE","WELCOME"];
-let tlPage1= gsap.timeline({paused:true});
-
-loadingArr.forEach((text,idx)=>{
-    setTimeout(()=>{
-        loadingText.innerText=text.toUpperCase();
-        if (loadingText.innerText==="welcome".toUpperCase()){
-            gsap.to(".l1",{
-                y:"-100%",
-                duration: 2,
-                ease: "expo.inOut"
-            });
-            gsap.to(".l2",{
-                y:"100%",
-                duration: 2,
-                ease:"expo.inOut",
-                onComplete: () => {
-                     gsap.set(".loading",{
-                        display:"none"
-                    });
-    
-                    tlPage1.play();
-                }
-            });
-        };
-    },idx*850);
-    
+//cursor 
+let cursor=document.querySelector(".cursor");
+window.addEventListener("mousemove",(dets)=>{
+    cursor.style.left=dets.clientX + "px";
+    cursor.style.top=dets.clientY + "px"; 
+});
+document.addEventListener("mouseleave",()=>{
+    cursor.style.opacity = "0"
+});
+document.addEventListener("mouseenter",()=>{
+    cursor.style.opacity = "1"
 });
 
-//page1
-tlPage1
-.from(".vline",{
-    scaleY:0,
-    duration:0.5,
-    stagger:0.2
-})
 
-.from(".hline",{
-    scaleX:0,
-    duration:0.5,
-    stagger:0.2
-},"<")
+// // loading animation
 
-.to(".title .fancyA, .title .fancyN",{
-    color:"rgb(131,41,214)",
-    duration:0.35,
-    ease:"power1.out"
-});
+// let loadingText=document.querySelector(".l2 h1");
+// let loadingArr=["PREPARING THE CONTENT","warming up the pixels","brewing the content","SUMMONING THE GOOD STUFF","MAKING THINGS LOOK EXPENSIVE","WELCOME"];
+// let tlPage1= gsap.timeline({paused:true});
+
+// loadingArr.forEach((text,idx)=>{
+//     setTimeout(()=>{
+//         loadingText.innerText=text.toUpperCase();
+//         if (loadingText.innerText==="welcome".toUpperCase()){
+//             gsap.to(".l1",{
+//                 y:"-100%",
+//                 duration: 2,
+//                 ease: "expo.inOut"
+//             });
+//             gsap.to(".l2",{
+//                 y:"100%",
+//                 duration: 2,
+//                 ease:"expo.inOut",
+//                 onComplete: () => {
+//                      gsap.set(".loading",{
+//                         display:"none"
+//                     });
+    
+//                     tlPage1.play();
+//                 }
+//             });
+//         };
+//     },idx*850);
+    
+// });
+
+// //page1
+// tlPage1
+// .from(".vline",{
+//     scaleY:0,
+//     duration:0.5,
+//     stagger:0.2
+// })
+
+// .from(".hline",{
+//     scaleX:0,
+//     duration:0.5,
+//     stagger:0.2
+// },"<")
+
+// .to(".title .fancyA, .title .fancyN",{
+//     color:"rgb(131,41,214)",
+//     duration:0.35,
+//     ease:"power1.out"
+// });
 
 
 
