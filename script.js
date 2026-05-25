589230321c52084165dc8a3a615657afb8941b03
Ayaan Khan <ayaankhan224271@gmail.com>
2026-05-25 17:28:30 +0530

    created front page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// loading anim
+
 let loadingText=document.querySelector(".l2 h1");
 let loadingArr=["PREPARING THE CONTENT","warming up the pixels","brewing the content","SUMMONING THE GOOD STUFF","MAKING THINGS LOOK EXPENSIVE","WELCOME"];
+let tlPage1= gsap.timeline({paused:true});
 
 loadingArr.forEach((text,idx)=>{
     setTimeout(()=>{
@@ -18,11 +21,33 @@ loadingArr.forEach((text,idx)=>{
                      gsap.set(".loading",{
                         display:"none"
                     });
+    
+                    tlPage1.play();
                 }
             });
         };
-    },idx*2000);
+    },idx*850);
     
+});
+
+//page1
+tlPage1
+.from(".vline",{
+    scaleY:0,
+    duration:0.5,
+    stagger:0.2
+})
+
+.from(".hline",{
+    scaleX:0,
+    duration:0.5,
+    stagger:0.2
+},"<")
+
+.to(".title .fancyA, .title .fancyN",{
+    color:"rgb(131,41,214)",
+    duration:0.35,
+    ease:"power1.out"
 });
 
 
