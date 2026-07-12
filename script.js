21cdb7b710ca2e3374b5b2d288f0303aeed40fa5
Ayaan Khan <ayaankhan224271@gmail.com>
2026-07-12 18:05:38 +0530

    fixed typo and changed skill section

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ let projects = document.querySelectorAll(".pro");
 let ogText = cursor.innerText;
 let projectInfo = "";
 let projectInfoArr = [
-  "ANKER: \nResumes Analuzer →",
+  "ANKER: \nResumes Analyzer →",
   "RADAR\nFind your skill gaps→",
   "this is project 3 \nabout xyz →",
   "this is project 4 \nabout xyz →",
